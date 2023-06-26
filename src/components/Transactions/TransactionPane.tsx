90092cbb8304e94cb9534fe487412eee37f7d506
Saurabh Kulkarni <srbhkulkarni13@gmail.com>
2023-06-26 00:17:38 +0000

    Solved Bug 7

--- src/components/Transactions/TransactionPane.tsx
+++ src/components/Transactions/TransactionPane.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { InputCheckbox } from "../InputCheckbox"
 import { TransactionPaneComponent } from "./types"
+import { useCustomFetch } from "../../hooks/useCustomFetch"
 
 export const TransactionPane: TransactionPaneComponent = ({
   transaction,
@@ -8,6 +9,7 @@ export const TransactionPane: TransactionPaneComponent = ({
   setTransactionApproval: consumerSetTransactionApproval,
 }) => {
   const [approved, setApproved] = useState(transaction.approved)
+  const { clearCache } = useCustomFetch()
 
   return (
     <div className="RampPane">
@@ -29,6 +31,7 @@ export const TransactionPane: TransactionPaneComponent = ({
           })
 
           setApproved(newValue)
+          clearCache()
         }}
       />
     </div>
